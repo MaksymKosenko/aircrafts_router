f13e04341cd3c15c3ffca839efced9d276c97388
deadmist666 <kosenkovladysl4v@gmail.com>
2023-10-31 18:30:18 +0200

    added mock routes added bloc provider for core data cubit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,12 @@
-import 'package:aircrafts_router/src/bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'algorithm_util/models/aircraft.dart';
 import 'algorithm_util/models/airport.dart';
 import 'algorithm_util/models/aircraft_route.dart';
-import 'bloc/core_data_bloc.dart';
+import 'bloc/core_data_cubit/core_data_cubit.dart';
 import 'features/sample_feature/sample_item_details_view.dart';
 import 'features/sample_feature/sample_item_list_view.dart';
 
@@ -50,71 +50,98 @@ class MyApp extends StatelessWidget {
         totalAircraftAmount: 3),
   ];
 
-  late final List<AircraftRoute> routes;
-  late final List<Aircraft> aircrafts;
-
-  @override
-  void initState() {
-    routes = [
-      AircraftRoute(
-        name: 'Route1',
-        startPoint: airports[0],
-        endPoint: airports[2],
-        routeProfit: 100,
-        routePriority: RoutePriority.low,
+  final List<AircraftRoute> routes = [
+    AircraftRoute(
+      name: "Route 1",
+      startPoint: Airport(
+        name: "Airport A",
+        airportPosition: AirportPosition(1, 2),
+        fuelAmount: 5000.0,
+        totalAircraftAmount: 10,
       ),
-      AircraftRoute(
-        name: 'Route2',
-        startPoint: airports[1],
-        //1 = b
-        endPoint: airports[4],
-        //4 = e
-        routeProfit: 130,
-        routePriority: RoutePriority.mid,
+      endPoint: Airport(
+        name: "Airport B",
+        airportPosition: AirportPosition(5, 8),
+        fuelAmount: 6000.0,
+        totalAircraftAmount: 12,
       ),
-      AircraftRoute(
-        name: 'Route3',
-        startPoint: airports[4],
-        endPoint: airports[1],
-        routeProfit: 230,
-        routePriority: RoutePriority.high,
+      routeProfit: 10000,
+      routePriority: RoutePriority.high,
+    ),
+    AircraftRoute(
+      name: "Route 2",
+      startPoint: Airport(
+        name: "Airport C",
+        airportPosition: AirportPosition(2, 3),
+        fuelAmount: 5500.0,
+        totalAircraftAmount: 8,
       ),
-      AircraftRoute(
-        name: 'Route3',
-        startPoint: airports[2],
-        endPoint: airports[1],
-        routeProfit: 200,
-        routePriority: RoutePriority.mid,
+      endPoint: Airport(
+        name: "Airport D",
+        airportPosition: AirportPosition(6, 9),
+        fuelAmount: 7000.0,
+        totalAircraftAmount: 14,
       ),
-    ];
-
-    aircrafts = [
-      Aircraft(
-        name: 'Aircraft1',
-        aircraftRoutes: [
-          routes[1],
-          routes[2],
-        ],
-        aircraftTechnicalState: AircraftTechnicalState.excellent,
-        fuelAmount: 21,
-        transportSpaceAmount: 50,
-        aircraftCost: 12345,
-        transportationResourceCost: 32,
-      ),
-      Aircraft(
-        name: 'Aircraft2',
-        aircraftRoutes: [
-          routes[0],
-          routes[3],
-        ],
-        aircraftTechnicalState: AircraftTechnicalState.good,
-        fuelAmount: 16,
-        transportSpaceAmount: 45,
-        aircraftCost: 12344,
-        transportationResourceCost: 90,
-      ),
-    ];
-  }
+      routeProfit: 12000,
+      routePriority: RoutePriority.critical,
+    ),
+  ];
+  final List<Aircraft> aircrafts = [
+    Aircraft(
+      name: "Boeing 747",
+      aircraftRoutes: [
+        AircraftRoute(
+          name: "Route 1",
+          startPoint: Airport(
+            name: "Airport A",
+            airportPosition: AirportPosition(1, 2),
+            fuelAmount: 5000.0,
+            totalAircraftAmount: 10,
+          ),
+          endPoint: Airport(
+            name: "Airport B",
+            airportPosition: AirportPosition(5, 8),
+            fuelAmount: 6000.0,
+            totalAircraftAmount: 12,
+          ),
+          routeProfit: 10000,
+          routePriority: RoutePriority.high,
+        ),
+      ],
+      aircraftTechnicalState: AircraftTechnicalState.good,
+      fuelAmount: 5000.0,
+      transportSpaceAmount: 300,
+      aircraftCost: 100000000,
+      transportationResourceCost: 1000,
+    ),
+    Aircraft(
+      name: "Airbus A380",
+      aircraftRoutes: [
+        AircraftRoute(
+          name: "Route 2",
+          startPoint: Airport(
+            name: "Airport C",
+            airportPosition: AirportPosition(2, 3),
+            fuelAmount: 5500.0,
+            totalAircraftAmount: 8,
+          ),
+          endPoint: Airport(
+            name: "Airport D",
+            airportPosition: AirportPosition(6, 9),
+            fuelAmount: 7000.0,
+            totalAircraftAmount: 14,
+          ),
+          routeProfit: 12000,
+          routePriority: RoutePriority.critical,
+        ),
+      ],
+      aircraftTechnicalState: AircraftTechnicalState.excellent,
+      fuelAmount: 6000.0,
+      transportSpaceAmount: 350,
+      aircraftCost: 120000000,
+      transportationResourceCost: 1200,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -125,62 +152,67 @@ class MyApp extends StatelessWidget {
     return AnimatedBuilder(
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
+        return BlocProvider(
+          create: (_) => CoreDataCubit(
+            airports: airports,
+            aircraftRoutes: routes,
+            aircrafts: aircrafts,
+          ),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            // Providing a restorationScopeId allows the Navigator built by the
+            // MaterialApp to restore the navigation stack when a user leaves and
+            // returns to the app after it has been killed while running in the
+            // background.
+            restorationScopeId: 'app',
 
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
+            // Provide the generated AppLocalizations to the MaterialApp. This
+            // allows descendant Widgets to display the correct translations
+            // depending on the user's locale.
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', ''), // English, no country code
+            ],
 
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
+            // Use AppLocalizations to configure the correct application title
+            // depending on the user's locale.
+            //
+            // The appTitle is defined in .arb files found in the localization
+            // directory.
+            onGenerateTitle: (BuildContext context) =>
+                AppLocalizations.of(context)!.appTitle,
 
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+            // Define a light and dark color theme. Then, read the user's
+            // preferred ThemeMode (light, dark, or system default) from the
+            // SettingsController to display the correct theme.
+            theme: ThemeData(),
+            darkTheme: ThemeData.dark(),
+            themeMode: settingsController.themeMode,
 
-          // Define a function to handle named routes in order to support
-          // Flutter web url navigation and deep linking.
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
-                  default:
-                    return BlocProvider<CoreDataBloc>(
-                        builder: (_) =>
-                            CoreDataBloc(airports, routes, aircrafts),
-                        child: SampleItemListView());
-                }
-              },
-            );
-          },
+            // Define a function to handle named routes in order to support
+            // Flutter web url navigation and deep linking.
+            onGenerateRoute: (RouteSettings routeSettings) {
+              return MaterialPageRoute<void>(
+                settings: routeSettings,
+                builder: (BuildContext context) {
+                  switch (routeSettings.name) {
+                    case SettingsView.routeName:
+                      return SettingsView(controller: settingsController);
+                    case SampleItemDetailsView.routeName:
+                      return const SampleItemDetailsView();
+                    case SampleItemListView.routeName:
+                    default:
+                      return const SampleItemListView();
+                  }
+                },
+              );
+            },
+          ),
         );
       },
     );
