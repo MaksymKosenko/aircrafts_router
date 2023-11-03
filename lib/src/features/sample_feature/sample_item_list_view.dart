5bfdae01ea1d37f784260924b9b729fc986877a0
deadmist666 <kosenkovladysl4v@gmail.com>
2023-11-03 23:52:45 +0200

    reorganized and refactored the entire widget  added core and flight simulation cubits  removed unnecessary code  created build functions for code clarity

--- lib/src/features/sample_feature/sample_item_list_view.dart
+++ lib/src/features/sample_feature/sample_item_list_view.dart
@@ -1,18 +1,16 @@
-import 'dart:async';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:aircrafts_router/src/algorithm_util/models/aircraft.dart';
 import 'package:aircrafts_router/src/algorithm_util/models/airport.dart';
-import 'package:aircrafts_router/src/algorithm_util/models/aircraft_route.dart';
-import 'package:flutter/material.dart';
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:aircrafts_router/src/bloc/aircraft_flight_simulation_cubit/aircraft_flight_simulation_cubit.dart';
+import 'package:aircrafts_router/src/bloc/core_data_cubit/core_data_cubit.dart';
+import 'package:aircrafts_router/src/ui/widgets/aircraft_widget.dart';
+import 'package:aircrafts_router/src/ui/widgets/airport_widget.dart';
+import 'package:aircrafts_router/src/settings/settings_view.dart';
+import 'package:aircrafts_router/src/ui/app_dimensions.dart';
 
-import '../../settings/settings_view.dart';
-import 'sample_item.dart';
-import 'sample_item_details_view.dart';
-
-/// Displays a list of SampleItems.
 class SampleItemListView extends StatefulWidget {
-  SampleItemListView({Key? key}) : super(key: key);
+  const SampleItemListView({Key? key}) : super(key: key);
 
   static const routeName = '/';
 
@@ -21,172 +19,14 @@ class SampleItemListView extends StatefulWidget {
 }
 
 class _SampleItemListViewState extends State<SampleItemListView> {
-  final List<Airport> items = [
-    Airport(
-        name: "A",
-        airportPosition: const AirportPosition(3, 1),
-        fuelAmount: 40,
-        totalAircraftAmount: 3),
-    Airport(
-        name: "B",
-        airportPosition: const AirportPosition(10, 6), //10; 6
-        fuelAmount: 40,
-        totalAircraftAmount: 3),
-    Airport(
-        name: "C",
-        airportPosition: const AirportPosition(1, 7),
-        fuelAmount: 40,
-        totalAircraftAmount: 3),
-    Airport(
-        name: "D",
-        airportPosition: const AirportPosition(5, 5),
-        fuelAmount: 40,
-        totalAircraftAmount: 3),
-    Airport(
-        name: "E",
-        airportPosition: const AirportPosition(6, 2), //6; 2
-        fuelAmount: 40,
-        totalAircraftAmount: 3),
-  ];
-
-  late final List<AircraftRoute> routes;
-  late final List<Aircraft> aircrafts;
+  late final CoreDataCubit coreDataCubit;
+  late final AircraftFlightSimulationCubit flightSimulationCubit;
 
   @override
   void initState() {
-    routes = [
-      AircraftRoute(
-        name: 'Route1',
-        startPoint: items[0],
-        endPoint: items[2],
-        routeProfit: 100,
-        routePriority: RoutePriority.low,
-      ),
-      AircraftRoute(
-        name: 'Route2',
-        startPoint: items[1],
-        //1 = b
-        endPoint: items[4],
-        //4 = e
-        routeProfit: 130,
-        routePriority: RoutePriority.mid,
-      ),
-      AircraftRoute(
-        name: 'Route3',
-        startPoint: items[4],
-        endPoint: items[1],
-        routeProfit: 230,
-        routePriority: RoutePriority.high,
-      ),
-      AircraftRoute(
-        name: 'Route3',
-        startPoint: items[2],
-        endPoint: items[1],
-        routeProfit: 200,
-        routePriority: RoutePriority.mid,
-      ),
-    ];
-
-    aircrafts = [
-      Aircraft(
-        name: 'Aircraft1',
-        aircraftRoutes: [
-          routes[1],
-          routes[2],
-        ],
-        aircraftTechnicalState: AircraftTechnicalState.excellent,
-        fuelAmount: 21,
-        transportSpaceAmount: 50,
-        aircraftCost: 12345,
-        transportationResourceCost: 32,
-      ),
-      Aircraft(
-        name: 'Aircraft2',
-        aircraftRoutes: [
-          routes[0],
-          routes[3],
-        ],
-        aircraftTechnicalState: AircraftTechnicalState.good,
-        fuelAmount: 16,
-        transportSpaceAmount: 45,
-        aircraftCost: 12344,
-        transportationResourceCost: 90,
-      ),
-    ];
     super.initState();
-  }
-
-  List<Positioned> trajectories = [];
-
-  Future<void>? moveThePlaneFromAtoB() {
-    Timer.periodic(const Duration(milliseconds: 250), (timer) {
-      setState(() {
-        aircrafts.first.currentPosition += 0.05;
-        aircrafts.last.currentPosition += 0.05;
-        trajectories
-          ..add(
-            Positioned(
-              left: getHorizontalPositionOfPlane(
-                  aircrafts
-                      .first.aircraftRoutes.first.startPoint.airportPosition.x,
-                  aircrafts
-                      .first.aircraftRoutes.first.endPoint.airportPosition.x,
-                  aircrafts.first.currentPosition),
-              top: getVerticalPositionOfPlane(
-                  aircrafts
-                      .first.aircraftRoutes.first.startPoint.airportPosition.y,
-                  aircrafts
-                      .first.aircraftRoutes.first.endPoint.airportPosition.y,
-                  aircrafts.first.currentPosition),
-              child: Container(
-                height: 4,
-                width: 4,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.redAccent),
-                  color: Colors.redAccent,
-                ),
-              ),
-            ),
-          )
-          ..add(Positioned(
-            left: getHorizontalPositionOfPlane(
-                aircrafts
-                    .last.aircraftRoutes.first.startPoint.airportPosition.x,
-                aircrafts
-                    .last.aircraftRoutes.first.endPoint.airportPosition.x,
-                aircrafts.first.currentPosition),
-            top: getVerticalPositionOfPlane(
-                aircrafts
-                    .last.aircraftRoutes.first.startPoint.airportPosition.y,
-                aircrafts
-                    .last.aircraftRoutes.first.endPoint.airportPosition.y,
-                aircrafts.last.currentPosition),
-            child: Container(
-              height: 4,
-              width: 4,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.blueAccent),
-                color: Colors.blueAccent,
-              ),
-            ),
-          ),);
-      });
-      if (aircrafts.first.currentPosition >= 1) {
-        timer.cancel();
-      }
-    });
-    return null;
-  }
-
-  Future<void>? restorePlanes() {
-    setState(() {
-      aircrafts.first.currentPosition = 0;
-      aircrafts.last.currentPosition = 0;
-      trajectories = [];
-    });
-    return null;
+    coreDataCubit = context.read<CoreDataCubit>();
+    flightSimulationCubit = context.read<AircraftFlightSimulationCubit>();
   }
 
   @override
@@ -195,130 +35,68 @@ class _SampleItemListViewState extends State<SampleItemListView> {
       appBar: AppBar(
         title: const Text('Sample Items'),
         actions: [
-          IconButton(
-              onPressed: () => moveThePlaneFromAtoB(),
-              icon: const Icon(Icons.play_arrow)),
-          IconButton(
-              onPressed: () => restorePlanes(),
-              icon: const Icon(Icons.restore_rounded)),
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
+          _buildPlayButton(),
+          _buildStopButton(),
+          _buildSettingsButton(),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Container(
-          child: Stack(
-            children: [
-              for (var airportPoint in items)
-                Positioned(
-                  left: getHorizontalPositionOfAirport(
-                      airportPoint.airportPosition.x),
-                  top: getVerticalPositionOfAirport(
-                      airportPoint.airportPosition.y),
-                  child: AirportWidget(airport: airportPoint),
-                ),
-              for (Aircraft aircarft in aircrafts)
-                Positioned(
-                  left: getHorizontalPositionOfPlane(
-                      aircarft
-                          .aircraftRoutes.first.startPoint.airportPosition.x,
-                      aircarft.aircraftRoutes.first.endPoint.airportPosition.x,
-                      aircarft.currentPosition),
-                  top: getVerticalPositionOfPlane(
-                      aircarft
-                          .aircraftRoutes.first.startPoint.airportPosition.y,
-                      aircarft.aircraftRoutes.first.endPoint.airportPosition.y,
-                      aircarft.currentPosition),
-                  child: AircraftWidget(aircraft: aircarft),
-                ),
-              for (Positioned trajectory in trajectories) trajectory,
-            ],
-          ),
+        padding: AppDimensions.horizontalPadding24,
+        child: BlocBuilder<AircraftFlightSimulationCubit,
+            AircraftFlightSimulationState>(
+          builder: (context, state) {
+            return _buildAircraftsAndAirports(state.aircraftList);
+          },
         ),
       ),
     );
   }
 
-  double getHorizontalPositionOfAirport(int airportPositionX) =>
-      airportPositionX.toDouble() * 50;
-
-  double getVerticalPositionOfAirport(int airportPositionY) =>
-      airportPositionY.toDouble() * 50;
-
-  double getHorizontalPositionOfPlane(int startAirportPositionX,
-      int endAirportPositionX, double currentPosition) {
-    int distanceBetween = (startAirportPositionX - endAirportPositionX).abs();
-
-    if (startAirportPositionX == endAirportPositionX) {
-      return startAirportPositionX * 50;
-    } else if (startAirportPositionX > endAirportPositionX) {
-      return (startAirportPositionX - currentPosition * distanceBetween) * 50;
-    } else {
-      return (startAirportPositionX + currentPosition * distanceBetween) * 50;
-    }
-  }
-
-  double getVerticalPositionOfPlane(int startAirportPositionY,
-      int endAirportPositionY, double currentPosition) {
-    int distanceBetween = (startAirportPositionY - endAirportPositionY).abs();
-    if (startAirportPositionY == endAirportPositionY) {
-      return startAirportPositionY * 50;
-    } else if (startAirportPositionY > endAirportPositionY) {
-      return (startAirportPositionY - currentPosition * distanceBetween) * 50;
-    } else {
-      return (startAirportPositionY + currentPosition * distanceBetween) * 50;
-    }
-  }
-}
-
-class AirportWidget extends StatefulWidget {
-  const AirportWidget({Key? key, required this.airport}) : super(key: key);
-
-  final Airport airport;
-
-  @override
-  State<AirportWidget> createState() => _AirportWidgetState();
-}
-
-class _AirportWidgetState extends State<AirportWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 64,
-      width: 64,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(),
-      ),
-      child: Center(child: Text(widget.airport.name)),
+  Widget _buildPlayButton() {
+    return IconButton(
+      onPressed: () {
+        if (flightSimulationCubit.simulationTimer == null ||
+            !flightSimulationCubit.simulationTimer!.isActive) {
+          flightSimulationCubit.startSimulation();
+        }
+      },
+      icon: const Icon(Icons.play_arrow),
     );
   }
-}
 
-class AircraftWidget extends StatelessWidget {
-  const AircraftWidget({Key? key, required this.aircraft}) : super(key: key);
-  final Aircraft aircraft;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(aircraft.name),
-           IconButton(
-            onPressed: null,
-            icon: Icon(Icons.airplanemode_active),//FaIcon(FontAwesomeIcons.plane),
-          )
-        ],
-      ),
+  Widget _buildStopButton() {
+    return IconButton(
+      onPressed: () {
+        flightSimulationCubit.stopSimulation();
+      },
+      icon: const Icon(Icons.restore_rounded),
     );
+  }
+
+  Widget _buildSettingsButton() {
+    return IconButton(
+      icon: const Icon(Icons.settings),
+      onPressed: () {
+        Navigator.restorablePushNamed(context, SettingsView.routeName);
+      },
+    );
+  }
+
+  Widget _buildAircraftsAndAirports(List<Aircraft> aircrafts) {
+    return Stack(
+      children: [
+        for (Airport airport in coreDataCubit.state.airports)
+          AirportWidget(airport: airport),
+        for (Aircraft aircraft in _getActiveAircrafts(aircrafts))
+          AircraftWidget(aircraft: aircraft),
+      ],
+    );
+  }
+
+  List<Aircraft> _getActiveAircrafts(List<Aircraft> aircrafts) {
+    return aircrafts
+        .where(
+            (aircraft) => aircraft.flightState != AircraftFlightState.completed)
+        .toList();
   }
 }
